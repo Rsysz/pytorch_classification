79c3fde17b8d2805aba370042b4988383d9f5965
yenpc <asd757817@gamil.com>
2020-11-18 07:19:33 +0000

    Fuck

--- cfg.py
+++ cfg.py
@@ -4,30 +4,21 @@
 # @author :lxztju
 # @github : https://github.com/lxztju
 import os
-home = os.path.expanduser('~')
-##数据集的类别
-NUM_CLASSES = 206
+home = '.'
 
-#训练时batch的大小
+NUM_CLASSES = 5
 BATCH_SIZE = 32
-
-#网络默认输入图像的大小
 INPUT_SIZE = 300
-#训练最多的epoch
 MAX_EPOCH = 100
-# 使用gpu的数目
-GPUS = 2
-# 从第几个epoch开始resume训练，如果为0，从头开始
+
+GPUS = 1
 RESUME_EPOCH = 0
 
 WEIGHT_DECAY = 5e-4
 MOMENTUM = 0.9
-# 初始学习率
 LR = 1e-3
 
-
-# 采用的模型名称
-model_name = 'resnext101_32x32d'
+model_name = 'efficientnet-b7'
 
 from models import Resnet50, Resnet101, Resnext101_32x8d,Resnext101_32x16d, Densenet121, Densenet169, Mobilenetv2, Efficientnet, Resnext101_32x32d, Resnext101_32x48d
 MODEL_NAMES = {
@@ -58,7 +49,7 @@ TEST_LABEL_DIR = BASE + 'test.txt'
 
 
 ##训练完成，权重文件的保存路径,默认保存在trained_model下
-TRAINED_MODEL = BASE + 'weights/resnext101_32x32d/epoch_40.pth'
+TRAINED_MODEL = BASE + 'weights/efficientnet/epoch_100.pth'
 
 
 
